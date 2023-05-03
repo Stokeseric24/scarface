9dd80a4ee2ebcc063c38a40250bc3e8fe636abdd
Stokeseric24 <132484049+Stokeseric24@users.noreply.github.com>
2023-05-03 12:32:22 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//The setup function only happens once
+var scarface=0;
+function setup() {
+	createCanvas(500, 500); //create a 500px X 500px canvas
+}
+
+//The draw function happens over and over again
+function draw() {
+  background(49, 104, 132 ); //an RGB color for the canvas' background
+  //circle
+  stroke(207, 93, 44 ) // an RGB color for the circle's border
+  fill(130,200,127,scarface); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  ellipse(mouseX,mouseY,200,150); // center of canvas, 20px dia
+  rect(150,scarface,150,200); 
+ 
+  fill(49,77,132)
+}
+
+
+function mousePressed()  { 
+if (scarface>=100)
+{sandy=0} else 
+{scarface=scarface+1;}
